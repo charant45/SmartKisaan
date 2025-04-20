6029dcfc28e7611c44985dcb60cc49a687401fba
charan <charan4567890@gmail.com>
2025-04-20 16:24:52 +0530

    update

--- djangoproject/myproject/myproject/settings.py
+++ djangoproject/myproject/myproject/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*ki!g6@xxk!gmhfafz$8t5r#9mm)kcatb^@cw!4=0f1e)6w*h'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0*ki!g6@xxk!gmhfafz$8t5r#9mm)kcatb^@cw!4=0f1e)6w*h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myproject.urls'
+
+
 
 TEMPLATES = [
     {
@@ -120,9 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'myapp/static'
+    BASE_DIR / 'myapp/static',
+    # BASE_DIR / 'crop_suggestions/static',  # Removed this line
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
